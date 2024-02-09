786fdde326cb58a5e5a56a7ef74de8667b37e2bf
Lefteris Kororos <lkororos@gmail.com>
2024-02-09 20:35:46 +0200

    Refactor layout in IndexPage.vue

--- admin_client/src/pages/IndexPage.vue
+++ admin_client/src/pages/IndexPage.vue
@@ -2,10 +2,10 @@
   <div class="flex row justify-center">
   <q-page class="column justify-center " >
     <summary-anemo-table class="q-ma-md"> </summary-anemo-table>
-    <div  class="row justify-between q-ma-md">
-      <firmware-info-table></firmware-info-table>
-      <q-card>
-        <firmware-upload > </firmware-upload>
+    <div  class="row  justify-between q-ma-md">
+      <firmware-info-table class="q-mr-md col-auto"></firmware-info-table>
+      <q-card class="q-ml-md col-auto">
+        <firmware-upload> </firmware-upload>
       </q-card>
     </div>
 
