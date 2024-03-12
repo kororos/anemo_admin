c47d30808ed1f8b1bc64f1f1f886df61e3781b69
Lefteris Kororos <lkororos@gmail.com>
2024-03-12 16:48:31 +0200

    Update package.json and index.js files

--- test_client/index.js
+++ test_client/index.js
@@ -1,12 +1,50 @@
 import { confirm, select, input } from "@inquirer/prompts";
 import WebSocket from "ws";
+import mqtt from "mqtt";
 
-let wsClient;
+let wsClient, prodWsClient;
 let data = {
   clientId: "",
   hwVersion: "",
   swVersion: "",
 };
+
+const prodSensorData ={
+  time: "anytime", // get epoch time
+  ver: "1.22.0_DHT22",
+  device: "Emulator",
+  sensors: [
+    {
+      sensor: "magneto",
+      data: "122",
+      min_x: "0",
+      max_x: "10",
+      min_y: "0",
+      max_y: "10",
+      min_z: "0",
+      max_z: "10"
+    },
+    {
+      sensor: "speed",
+      data: {
+        data: "10", //meters per second, 
+        rotpersec: "7", //rotations per second
+      } 
+    },
+    {
+      sensor: "temperature",
+      data: "20"
+    },
+    {
+      sensor: "humidity",
+      data: "50"
+    },
+    {
+      sensor: "analogue",
+      data: "0.5"
+    }
+  ]
+}
 
 const questions = {
   message: "Issue another command?",
@@ -20,6 +58,9 @@ const selectCommand = {
     { title: "Connect", value: "Connect" },
     { title: "Disconnect", value: "Disconnect" },
     { title: "Get latest version", value: "Get latest version" },
+    { title: "Connect to PRODUCTION WS", value: "Connect to PRODUCTION WS"},
+    { title: "Connect to PRODUCTION MQTT", value: "Connect to PRODUCTION MQTT"}
+
   ],
 };
 
@@ -43,6 +84,63 @@ async function askCont() {
     wsClient.close();
     return false;
   }
+} 
+
+async function connetToProdMqtt(){
+  const client = mqtt.connect(process.env.MQTT_PROD_BASE_URL)
+  
+  client.on("connect", () => {
+    console.log("Connected to PRODUCTION MQTT");
+    //const data = `anemometer,device=Emulator temp=20,hummidity=50,rotPerSec=7,direction=122,version="1.22.0_DHT22"`;
+    //client.publish("sensors", data);
+    setInterval(() => {
+      let temp = Math.random() * 40;
+      let hummidity = Math.random() * 100;
+      let rotPerSec = Math.random() * 20;
+      let direction = Math.random() * 360;
+      let version = "1.22.0_DHT22";
+      const data = `anemometer,device=Emulator temp=${temp},hummidity=${hummidity},rotPerSec=${rotPerSec},direction=${direction},version="${version}"`;
+      //const data = `anemometer,device=Emulator temp=20,hummidity=50,rotPerSec=7,direction=122,version="1.22.0_DHT22"`;
+      client.publish("sensors", data);
+    }, 1000);
+  });
+  client.on("error", (err) => {
+    console.log(`Error ${err}MQTT`);
+  });
+}
+
+
+
+async function connectProdWs(){
+  if (prodWsClient == undefined) {
+    prodWsClient = new WebSocket(`${process.env.WS_PROD_BASE_URL}?clientId=${data.clientId}&hwVersion=${data.hwVersion}&swVersion=${data.swVersion}`);
+  } else {
+    if (prodWsClient.readyState === WebSocket.OPEN)
+      console.log("Connection already open");
+    else prodWsClient = new WebSocket(`${process.env.WS_PROD_BASE_URL}?clientId=${data.clientId}&hwVersion=${data.hwVersion}&swVersion=${data.swVersion}`);
+  }
+  prodWsClient.on("open", () => {
+    console.log("[PRODUCTION WS]: Connected")
+      setInterval(() => {
+      //console.log("[PRODUCTION WS]: Sending data");
+      //console.log("[PRODUCTION WS]: Data sent: ", JSON.stringify(prodSensorData));
+      prodWsClient.send(JSON.stringify(prodSensorData));
+    }, 1000);
+  });
+  prodWsClient.on("close", () => console.log("[PRODUCTION WS]: Disconnected"));
+  prodWsClient.on("message", async (message) => {
+    console.log("[PRODUCTION WS]: Got the following data: ", message.toString());
+    switch (message.toString()){
+      case "restart" :
+        await prodWsClient.close();
+        connectProdWs();
+    }
+  }
+  );
+  prodWsClient.on("error", (err) => {
+      console.log(`[PRODUCTION WS]: Error ${err}WebSocket`);
+  });
+
 }
 
 async function connectWs() {
@@ -93,6 +191,24 @@ async function ask() {
       };
       
       await connectWs();
+      break;
+    case "Connect to PRODUCTION WS":
+      console.log("\nWill now connect to PRODUCTION websocket server");
+      const clientIdProd = await input(clientIdQuestion);
+      const hwVersionProd = await input(hwVersionQuestion);
+      const swVersionProd = await input(swVersionQuestion);
+
+      data = {
+        clientId: clientIdProd,
+        hwVersion: hwVersionProd,
+        swVersion: swVersionProd,
+      };
+      
+      await connectProdWs();
+      break;
+    case "Connect to PRODUCTION MQTT":
+      console.log("\nWill now connect to PRODUCTION MQTT server");
+      connetToProdMqtt();
       break;
     case "Disconnect":
       console.log("\nWill now Disconnect from websocket server");
