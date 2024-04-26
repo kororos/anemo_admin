b820b383771bf0cf8add54aa208c0d8b81c5a9df
Lefteris Kororos <lkororos@gmail.com>
2024-04-26 20:10:59 +0300

    Update package.json dependencies and add firmware file upload functionality in FirmwareUpload.vue

--- admin_client/src/components/FirmwareUpload.vue
+++ admin_client/src/components/FirmwareUpload.vue
@@ -3,7 +3,7 @@
     <q-form @submit.prevent="submitForm" class="q-gutter-md">
       <q-input v-model="hwVersion" id="hwVersion" label="Hardware Version" outlined dense></q-input>
       <q-input v-model="swVersion" id="swVersion" label="Software Version" outlined dense></q-input>
-      <q-file dense outlined bottom-slots v-model="path" id="firmwareFile" label="Firmware File" counter>
+      <q-file dense outlined bottom-slots @update:model-value="fileSelected" v-model="path" id="firmwareFile" label="Firmware File" counter>
         <template v-slot:prepend>
           <q-icon name="cloud_upload" @click.stop.prevent />
         </template>
@@ -24,6 +24,8 @@
 import { ref } from 'vue';
 import { api } from '../boot/axios.js';
 import { useQuasar } from 'quasar';
+import { isBrowser, isNode } from 'browser-or-node';
+import * as zip from "@zip.js/zip.js";
 
 const $q = useQuasar();
 const hwVersion = ref('');
@@ -32,6 +34,31 @@ const path = ref(null);
 const uploadProgress = ref(0);
 const emit = defineEmits(['progressUpload', 'uploadFinished', 'uploadError']);
 
+function fileSelected(file) {
+  console.log("File selected: ",file);
+  console.log(isBrowser? "Browser" : "Node");
+  const zipFile = new zip.ZipReader(new zip.BlobReader(file));
+  zipFile.getEntries().then((entries) => {
+    console.log(entries);
+    // If the filename is descriptor.txt read the file and parse the contents
+    entries.forEach(async (entry) => {
+      if (entry.filename === 'descriptor.txt') {
+        const text = await entry.getData(new zip.TextWriter());
+        // Split the text by new line. For each line read the key value pair
+        const lines = text.split('\n');
+        for (const line of lines) {
+          const [key, value] = line.split('=');
+          if (key === 'hwVersion') {
+            hwVersion.value = value;
+          } else if (key === 'swVersion') {
+            swVersion.value = value;
+          }
+        }
+      }
+    });
+
+  });
+}
 
 function enabled() {
   return hwVersion.value && swVersion.value && path.value;
@@ -41,9 +68,19 @@ async function submitForm() {
   const formData = new FormData();
   formData.append('hwVersion', hwVersion.value);
   formData.append('swVersion', swVersion.value);
-  formData.append('file', path.value);
+  //formData.append('file', path.value);
 
-
+  // Unzip the file and read the firmware.bin file. Send this file to the api/formwareUpload endpoint
+  const zipFile = await new zip.ZipReader(new zip.BlobReader(path.value));
+  const entries = await zipFile.getEntries();
+  for (const entry of entries) {
+    if (entry.filename === 'firmware.bin') {
+      const fileData = await entry.getData(new zip.BlobWriter());
+      formData.append('file', fileData, 'firmware.bin');
+      break;
+    }
+  }
+  await zipFile.close();
   const response = await api.post('/api/firmwareUpload', formData, {
     headers: {
       'Content-Type': 'multipart/form-data'
