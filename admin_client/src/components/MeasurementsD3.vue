e414b5ca5fd0c8e26a2ce136b06b29a5795ab4ee
Lefteris Kororos <lkororos@gmail.com>
2024-05-10 23:27:44 +0300

    Adjust chart transformations and update wind direction visualization in LivePage.vue and MeasurementsD3.vue

--- admin_client/src/components/MeasurementsD3.vue
+++ admin_client/src/components/MeasurementsD3.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script setup>
-import { ref, computed, onMounted, onBeforeUpdate } from 'vue';
+import { ref, computed, onMounted, onBeforeMount, onBeforeUpdate } from 'vue';
 import * as d3 from 'd3';
 import { InfluxDB } from '@influxdata/influxdb-client-browser'
 
@@ -47,11 +47,19 @@ const humidityMeasurements = computed(() => {
 const directionMeasurements = computed(() => {
     return measurements.value.filter(m => m._field === 'direction');
 });
+onBeforeMount(async () => {
+
+    //    createChart();
+});
 onMounted(async () => {
+    //   await iterateRows();
     await iterateRows();
-    updateChart();
+    updateFunctions();
+    createChart();
     setInterval(async () => {
         await iterateRows();
+        updateFunctions();
+        updateChart();
     }, 10000);
 });
 async function iterateRows() {
@@ -64,35 +72,50 @@ async function iterateRows() {
     }
 }
 
+const width = 400;
+const height = 130;
+const margin = { top: 10, right: 30, bottom: 30, left: 30 };
 
-function updateChart() {
-    const xAxis = d3.scaleUtc(d3.extent(measurements.value, d => d._time), [0, 340]);
-    const yAxis = d3.scaleLinear([0, d3.max(tempMeasurements.value, d => d._value)], [100, 0]);
-    const y2Axis = d3.scaleLinear([0, d3.max(speedMeasurements.value, d => d._value)], [100, 0]);
-    const line = d3.line()
+
+let xAxis;
+let yAxis;
+let y2Axis;
+let line;
+let speedLine;
+let area;
+let speedArea;
+
+function updateFunctions() {
+    xAxis = d3.scaleUtc().range([0, width - margin.left - margin.right]);
+    xAxis.domain(d3.extent(tempMeasurements.value, d => d._time));
+    //xAxis.domain(d3.extent(tempMeasurements.value, d => d._time));
+    yAxis = d3.scaleLinear([0, 45 /* d3.max(tempMeasurements.value, d => d._value)*/], [height - margin.bottom, 0]);
+    y2Axis = d3.scaleLinear([0, 22.5 /*d3.max(speedMeasurements.value, d => d._value)*/], [height - margin.bottom, 0]);
+    line = d3.line()
         .x(d => xAxis(d._time))
         .y(d => yAxis(d._value));
 
-    const speedLine = d3.line()
+    speedLine = d3.line()
         .x(d => xAxis(d._time))
         .y(d => y2Axis(d._value));
 
-    const area = d3.area()
+    area = d3.area()
         .x(d => xAxis(d._time))
         .y0(yAxis(0))
         .y1(d => yAxis(d._value))
         .curve(d3.curveBasis);
 
-    const speedArea = d3.area()
+    speedArea = d3.area()
         .x(d => xAxis(d._time))
         .y0(y2Axis(0))
         .y1(d => y2Axis(d._value))
         .curve(d3.curveBasis);
-
+}
+function createChart() {
     const svg = d3.select(`.${props.id}`)
         .attr('width', "100%")
         .attr('height', "100%")
-        .attr('viewBox', '0 0 400 130')
+        .attr('viewBox', `0 0 ${width} ${height}`)
         .attr('preserveAspectRatio', 'xMidYMid meet');
 
     const defs = svg.append('defs');
@@ -120,25 +143,29 @@ function updateChart() {
         .attr('stop-color', 'green')
         .attr('stop-opacity', 0.1);
 
-    svg.append('path')
+    svg.append('g')
+        .append('path')
+        .attr('id', 'speedArea')
+        .data([speedMeasurements.value])
         .attr('fill', 'url(#speed-gradient)')
         //.attr('fill-opacity', 0.3)
         .attr('stroke', 'green')
         .attr('stroke-width', 0.5)
-        .attr('d', speedArea(speedMeasurements.value))
-        .attr('transform', `translate(30, 0)`);
+        .attr('d', speedArea)
+        .attr('transform', `translate(${margin.left}, 0)`);
 
     svg.append('path')
+        .attr('id', 'tempArea')
+        .data([tempMeasurements.value])
         .attr('fill', 'url(#temperature-gradient)')
-        //.attr('fill-opacity', 0.3)
         .attr('stroke', 'steelblue')
         .attr('stroke-width', 0.5)
-        .attr('d', area(tempMeasurements.value))
-        .attr('transform', `translate(30, 0)`)
+        .attr('d', d => area(d))
+        .attr('transform', `translate(${margin.left}, 0)`)
         .on('mouseover', function (event, d) {
             d3.select("div.tip")
                 .style('opacity', 1)
-                .style('left', `${d3.pointer(event)[0]+60}px`)
+                .style('left', `${d3.pointer(event)[0] + 60}px`)
                 .style('top', `${d3.pointer(event)[1]}px`);
         })
         .on('mouseout', function (event, d) {
@@ -156,28 +183,29 @@ function updateChart() {
                 .style('opacity', 1)
                 .style('left', `${event.pageX}px`)
                 .style('top', `${event.pageY}px`)
-                .html(`Temperature: ${Math.round(tempDataPoint._value* 10, )/10}°C<br/>\
-                Speed: ${Math.round(speedDataPoint._value*10)/10}kts`);
+                .html(`Temperature: ${Math.round(tempDataPoint._value * 10,) / 10}°C<br/>\
+                Speed: ${Math.round(speedDataPoint._value * 10) / 10}kts`);
         });
 
     svg.append('g')
-        .attr('transform', `translate(30, 0)`)
-        .call(d3.axisLeft(yAxis).tickSize(3).tickSizeOuter(0).ticks(10).tickFormat(d => `${d}°C`))
-        .call(g => g.select('.domain').style('stroke-width', 0.5))
-        .call(g => g.selectAll('.tick line').style('stroke-width', 0.5))   
-        .selectAll('text')
-        .style('font-size', '6px');
-
-    svg.append('g')
-        .attr('transform', `translate(370,0)`)
-        .call(d3.axisRight(y2Axis).tickSize(3).tickSizeOuter(0).ticks(5).tickFormat(d => `${d}kts`))
+        .attr('transform', `translate(${margin.left}, 0)`)
+        .call(d3.axisLeft(yAxis).tickValues([0, 5, 10, 15, 20, 25, 30, 35, 40]).tickSize(3).tickSizeOuter(0).tickFormat(d => `${d}°C`))
         .call(g => g.select('.domain').style('stroke-width', 0.5))
         .call(g => g.selectAll('.tick line').style('stroke-width', 0.5))
         .selectAll('text')
         .style('font-size', '6px');
 
     svg.append('g')
-        .attr('transform', `translate(30, 100)`)
+        .attr('transform', `translate(${width - margin.right},0)`)
+        .call(d3.axisRight(y2Axis).tickValues([0, 5, 10, 15, 20]).tickSize(3).tickSizeOuter(0).ticks(5).tickFormat(d => `${d}kts`))
+        .call(g => g.select('.domain').style('stroke-width', 0.5))
+        .call(g => g.selectAll('.tick line').style('stroke-width', 0.5))
+        .selectAll('text')
+        .style('font-size', '6px');
+
+    svg.append('g')
+        .attr('id', 'xAxis')
+        .attr('transform', `translate(${margin.left}, ${height - margin.bottom})`)
         .call(d3.axisBottom(xAxis).tickSize(2).tickSizeOuter(0).ticks(d3.timeMinute.every(30)).tickFormat(d3.timeFormat('%H:%M')))
         .call(g => g.select('.domain').style('stroke-width', 0.5))
         .call(g => g.selectAll('.tick line').style('stroke-width', 0.5))
@@ -198,5 +226,26 @@ function updateChart() {
         .style('border-radius', '5px')
         .style('font-size', '10px')
         .style('pointer-events', 'none');
+}
+
+function updateChart() {
+    xAxis.domain(d3.extent(measurements.value, d => d._time));
+    d3.select('#xAxis')
+        .call(d3.axisBottom(xAxis).tickSize(2).tickSizeOuter(0).ticks(d3.timeMinute.every(30)).tickFormat(d3.timeFormat('%H:%M')))
+        .call(g => g.select('.domain').style('stroke-width', 0.5))
+        .call(g => g.selectAll('.tick line').style('stroke-width', 0.5))
+        .call(g => g.selectAll('.tick text').style('font-size', '6px'))
+        .selectAll('text')
+        .attr('transform', 'translate(-7, 15) rotate(-90)');
+
+    d3.select('#tempArea')
+        .data([tempMeasurements.value])
+        .join('path')
+            .attr('d', d => area(d));
+
+    d3.selectAll('#speedArea')
+        .data([speedMeasurements.value])
+        .join('path')
+            .attr('d', d => speedArea(d));
 }
 </script>
