8273acba996ad0c11a173bb73c1b78eaa08d63e9
Lefteris Kororos <lkororos@gmail.com>
2025-04-14 14:51:23 +0300

    Update docker_build.sh to use envsubst for environment variable substitution in compose file

--- admin_client/scripts/docker_build.sh
+++ admin_client/scripts/docker_build.sh
@@ -8,6 +8,7 @@ DOCKERFILE_PATH="../."
 DOCKER_REGISTRY="registry.kororos.eu/anemometer"
 STACK_NAME="anemometer"
 COMPOSE_FILE="/home/$SUDO_USER/compose/anemometer/docker-compose-anemometer.yml"
+COMPOSE_FILE_WITH_ENV_VARS="./compose-with-env-vars.yml"
 
 # Build the Docker image with version as build arg
 docker build --build-arg APP_VERSION=$VERSION -t $DOCKER_REGISTRY/$IMAGE_NAME:$TAG $DOCKERFILE_PATH
@@ -19,5 +20,11 @@ docker tag $DOCKER_REGISTRY/$IMAGE_NAME:$TAG $DOCKER_REGISTRY/$IMAGE_NAME:latest
 docker push $DOCKER_REGISTRY/$IMAGE_NAME:$TAG
 docker push $DOCKER_REGISTRY/$IMAGE_NAME:latest
 
+
+export APP_VERSION=$VERSION
+envsubst < $COMPOSE_FILE > $COMPOSE_FILE_WITH_ENV_VARS
+
 # Update the stack
-docker stack deploy -c $COMPOSE_FILE --with-registry-auth $STACK_NAME
+docker stack deploy -c $COMPOSE_FILE_WITH_ENV_VARS --with-registry-auth $STACK_NAME
+
+rm $COMPOSE_FILE_WITH_ENV_VARS
